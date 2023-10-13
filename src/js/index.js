5753ca15d3fb888a30999489c537e55caec85302
dddmogui13 <dmytropotorocha213@gmail.com>
2023-10-13 23:41:08 +0300

    ddd

--- src/js/index.js
+++ src/js/index.js
@@ -7,89 +7,85 @@ const breedSelect = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
 const catInfo = document.querySelector('.cat-info');
+let arrOptions = [{ text: '', value: '', placeholder: true }];
 
-const arrOptions = [{ text: '', value: '', placeholder: true }];
-
-function addBreedOptions(breeds) {
-  breeds.forEach((breed) => {
-    arrOptions.push({
-      text: breed.name,
-      value: breed.id,
-      style: 'background-color: YellowGreen',
-    });
-  });
-}
-
-function updateBreedSelect() {
-  new SlimSelect({
-    select: breedSelect,
-    data: arrOptions,
-    settings: {
-      allowDeselect: true,
-    },
-  });
-}
-
-function displayCatInfo(data) {
-  const cat = data[0].breeds[0];
-  catInfo.innerHTML = `
-    <div>
-      <img src="${data[0].url}" alt="${cat.name}" width="400" />
-    </div>
-    <div>
-      <b><h2>${cat.name}</h2></b>
-      <p>${cat.description}</p>
-      <p><b>Temperament:</b> ${cat.temperament}</p>
-    </div>
-  `;
-}
-
-function handleErrors(err) {
-  Notify.failure('Oops! Something went wrong! Try reloading the page!');
-  changeErrorStatus();
-  changeLoadingStatus();
-}
-
-function changeLoadingStatus() {
-  loader.classList.replace('loader', 'unloader');
-}
-
-function changeErrorStatus() {
-  error.classList.replace('error', 'unloader');
-}
-
-function initialize() {
+const initialize = () => {
+  breedSelect.classList.replace('breed-select', 'unloader');
   breedSelect.style.color = '#008000';
   breedSelect.style.maxWidth = '400px';
-
   catInfo.style.display = 'flex';
   catInfo.style.gap = '20px';
 
-  breedSelect.classList.replace('breed-select', 'unloader');
-  changeErrorStatus();
-
+ 
   fetchBreeds()
-    .then((breeds) => {
-      addBreedOptions(breeds);
-      updateBreedSelect();
+    .then((data) => {
+      arrOptions = [
+        ...arrOptions,
+        ...data.map((element) => ({
+          text: element.name,
+          value: element.id,
+          style: 'background-color: YellowGreen',
+        })),
+      ];
+
+      new SlimSelect({
+        select: breedSelect,
+        data: arrOptions,
+        settings: {
+          allowDeselect: true,
+        },
+      });
+
       changeLoadingStatus();
       breedSelect.classList.replace('unloader', 'breed-select');
     })
     .catch(handleErrors);
+};
 
-  breedSelect.addEventListener('change', () => {
-    changeErrorStatus();
+const handleErrors = (err) => {
+  Notify.failure('Oops! Something went wrong! Try reloading the page!');
+  changeErrorStatus();
+  changeLoadingStatus();
+};
+
+const updateCatInfo = (data) => {
+  changeLoadingStatus();
+  const breed = data[0].breeds[0];
+  catInfo.innerHTML = `<div><img src="${data[0].url}" alt="${breed.name}" width="400" /></div>
+    <div><b><h2>${breed.name}</h2></b>
+    <p>${breed.description}</p>
+    <p><b>Temperament:</b> ${breed.temperament}</p></div>`;
+};
+
+const changeLoadingStatus = () => {
+  loader.classList.replace('loader', 'unloader');
+};
+
+const changeUnLoadingStatus = () => {
+  loader.classList.replace('unloader', 'loader');
+};
+
+const changeUnErrorStatus = () => {
+  error.classList.replace('unloader', 'error');
+};
+
+const changeErrorStatus = () => {
+  error.classList.replace('error', 'unloader');
+};
+
+const onBreedSelectChange = () => {
+  changeErrorStatus();
+  changeUnLoadingStatus();
+  const breedId = breedSelect.value;
+  if (breedId !== '') {
+    fetchCatByBreed(breedId)
+      .then(updateCatInfo)
+      .catch(handleErrors);
+  } else {
     changeLoadingStatus();
-    const breedId = breedSelect.value;
-    if (breedId !== '') {
-      fetchCatByBreed(breedId)
-        .then(displayCatInfo)
-        .catch(handleErrors);
-    } else {
-      catInfo.innerHTML = '';
-      changeLoadingStatus();
-    }
-  });
-}
+    catInfo.innerHTML = '';
+  }
+};
 
 initialize();
+breedSelect.addEventListener('change', onBreedSelectChange);
